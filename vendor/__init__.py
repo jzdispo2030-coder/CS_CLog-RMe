74e9e1c7d0742a50c0347fdb5a2406bd8433c68e
jzdispo2030-coder <jzdispo2030@crc.pshs.edu.ph>
2026-03-20 16:53:54 +0800

    Update __init__.py

--- vendor/__init__.py
+++ vendor/__init__.py
@@ -1,1 +1,8 @@
+# vendor/__init__.py
+import os
+import sys
 
+# Add the vendor directory to Python's import path
+vendor_dir = os.path.dirname(__file__)
+if vendor_dir not in sys.path:
+    sys.path.insert(0, vendor_dir)
